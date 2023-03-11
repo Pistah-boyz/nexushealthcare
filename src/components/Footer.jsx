5569b307cbf388eea80fdbd1e285ba56d32134bc
gowths20 <gowtham2indian@gmail.com>
2023-03-11 18:35:34 +0530

    main screen plus footer completed
    added why choose page as per client requirements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,101 @@
 import React from "react";
 
-import { Box, Typography, Grid } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Grid,
+  Card,
+  FormControl,
+  TextField,
+  IconButton,
+  Button,
+} from "@mui/material";
+import {
+  Place as LocationIcon,
+  Phone as PhoneIcon,
+  Facebook as FacebookIcon,
+  Instagram as InstagramIcon,
+  Twitter as TwitterIcon,
+  LinkedIn as LinkedinIcon,
+} from "@mui/icons-material";
+import TextareaAutosize from "@mui/base/TextareaAutosize";
 import { makeStyles } from "@mui/styles";
 
-import img2 from "../images/action-bg.jpg";
+import { Container } from "../ui";
+import { COLORS } from "../ui/Theme/colors";
+
+import MyLogo from "../images/Nexus.jpeg";
+import { BackgroundComponent } from "./auth/BackgroundComponent";
+import card1 from "../images/card1.jpg";
+import card2 from "../images/card2.png";
+import card3 from "../images/card3.jpg";
+import card4 from "../images/card4.jpg";
+import card5 from "../images/card5.jpg";
+import card6 from "../images/card6.jpg";
+
+const styles = {
+  offerContainer: {
+    padding: "24px",
+    background: COLORS.primary,
+    color: COLORS.white,
+  },
+  offer: {
+    position: "relative",
+    width: "100%",
+    maxHeight: "250px",
+    borderRadius: "10px",
+    background: "none",
+
+    "&:hover .overlay": {
+      backgroundColor: "rgb(0 65 101 / 30%)",
+    },
+
+    "&:hover .subTitle": {
+      display: "block",
+    },
+
+    "&:hover .content": {
+      border: "none",
+      backgroundColor: "none",
+    },
+  },
+
+  media: {
+    width: "100%",
+  },
+  card: {
+    position: "relative",
+  },
+  overlay: {
+    position: "absolute",
+    bottom: "0",
+    right: "0",
+    margin: "10px",
+    padding: "8px",
+    color: COLORS.white,
+    borderRadius: "10px",
+    backgroundColor: "rgb(0 65 101 / 70%)",
+  },
+  boxOverlay: {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    borderRadius: "10px",
+  },
+  address: {
+    padding: "40px",
+    background: "#4676b0",
+    color: COLORS.white,
+  },
+  contact: {
+    padding: "40px",
+  },
+  footerIcons: {
+    color: COLORS.white,
+  },
+};
 
 const useStyles = makeStyles({
   imgtranscolor: {
@@ -46,52 +138,214 @@ const useStyles = makeStyles({
   },
 });
 
+const offers = [
+  { title: "STAFFING", subTitle: "VIEW SERVICES", img: card1 },
+  { title: "GOVERMENT", subTitle: "VIEW SERVICES", img: card2 },
+  { title: "CANDIDATES", subTitle: "LEARN MORE", img: card3 },
+  { title: "EMPLOYERS", subTitle: "LEARN MORE", img: card4 },
+  { title: "JOB BOARD", subTitle: "VIEW OPEN POSITIONS", img: card5 },
+  { title: "APPLY NOW!", subTitle: "LETS GET STARTED", img: card6 },
+];
+
 const Footer = () => {
   const classes = useStyles();
   return (
-    <Box
-      className={classes.imgtranscolor}
-      sx={{
-        backgroundImage: `url(${img2})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        position: "relative",
-        height: 730,
-      }}
-    >
-      <Box
-        sx={{
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          paddingBottom: "10rem",
-          paddingLeft: "10rem",
-          alignItems: "flex-end",
-          justifyContent: "center",
-        }}
-      >
-        <Box>
-          <Typography
-            variant="h3"
-            color="white"
-            className={classes.section2_h3}
-          >
-            PUT OUR EXTENSIVE NETWORK OF PARTNERS AND RESOURCES TO WORK FOR YOU!
-          </Typography>
-          <Typography
-            component="p"
-            color="white"
-            className={classes.section2_p}
-          >
-            Whether you are interested in a temporary assignment, per diem work,
-            or a permanent role, our team is dedicated to learning who you are
-            on both a personal and professional level. Let Ellsworth Healthcare
-            Staffing streamline the process for you.
-          </Typography>
-        </Box>
+    <>
+      <BackgroundComponent />
+      <Box sx={styles.offerContainer}>
+        <Container maxWidth={"lg"} disableGutters="false">
+          <Box>
+            <Typography variant="h3">
+              WHAT <span style={{ fontWeight: "bold" }}>WE OFFER</span>
+            </Typography>
+            <hr />
+          </Box>
+          <Box sx={{ padding: "16px" }}>
+            <Typography variant="h6">
+              Ellsworth Healthcare Staffing offers a comprehensive suite of
+              services designed to support local healthcare facilities, connect
+              the best applicants with each opportunity, and deliver the best
+              care to patients across Atlanta.
+            </Typography>
+          </Box>
+          <Box sx={{ padding: "16px" }}>
+            <Grid container spacing={8}>
+              {offers.map((val) => (
+                <Grid item md={4}>
+                  <Card sx={styles.offer}>
+                    <img src={val.img} style={styles.media} />
+                    <Box className={"content"} style={styles.overlay}>
+                      <Typography variant="h6">{val.title}</Typography>
+                      <Typography
+                        className={"subTitle"}
+                        variant="h6"
+                        sx={{ fontWeight: "bold", display: "none" }}
+                      >
+                        {val.subTitle}
+                      </Typography>
+                    </Box>
+                    <Box className={"overlay"} style={styles.boxOverlay}></Box>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Container>
       </Box>
-    </Box>
+
+      <Grid container>
+        <Grid item md={6} sx={styles.address}>
+          <Box sx={{ padding: "16px" }}>
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{ color: COLORS.WHITE }}
+            >
+              NEXUS HEALTHCARE{" "}
+              <span style={{ fontWeight: "bold" }}>STAFFING</span>
+            </Typography>
+          </Box>
+          <Box sx={{ padding: "16px" }}>
+            <Typography variant="h6">
+              We welcome healthcare facilities and healthcare professionals
+              looking for employment opportunities into our trusted network. Get
+              in touch with us today to learn more about Ellsworth Healthcare
+              Staffing.
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center", padding: "16px" }}>
+            <LocationIcon />
+            <Typography variant="h5" sx={{ marginLeft: "8px" }}>
+              160 Clairemont Ave, Suite 200 Decatur, GA 30030
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center", padding: "16px" }}>
+            <PhoneIcon />
+            <Typography variant="h5" sx={{ marginLeft: "8px" }}>
+              404-806-8164
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", padding: "16px" }}>
+            <IconButton sx={styles.footerIcons}>
+              <FacebookIcon />
+            </IconButton>
+
+            <IconButton sx={styles.footerIcons}>
+              <InstagramIcon />
+            </IconButton>
+
+            <IconButton sx={styles.footerIcons}>
+              <TwitterIcon />
+            </IconButton>
+
+            <IconButton sx={styles.footerIcons}>
+              <LinkedinIcon />
+            </IconButton>
+          </Box>
+        </Grid>
+        <Grid item md={6} sx={styles.contact}>
+          <Box sx={{ padding: "16px" }}>
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{ fontWeight: "bold" }}
+            >
+              CONTACT US FOR MORE INFORMATION
+            </Typography>
+          </Box>
+          <Box sx={{ padding: "16px" }}>
+            <Typography variant="h6">
+              We welcome healthcare facilities and healthcare professionals
+              looking for employment opportunities into our trusted network. Get
+              in touch with us today to learn more about Ellsworth Healthcare
+              Staffing.
+            </Typography>
+          </Box>
+          <Box sx={{ padding: "16px" }}>
+            <FormControl sx={{ width: "100%" }}>
+              <TextField
+                id="name"
+                label="Name"
+                variant="outlined"
+                fullWidth
+                sx={{ margin: "8px" }}
+              />
+              <TextField
+                id="email"
+                label="Email"
+                variant="outlined"
+                fullWidth
+                sx={{ margin: "8px" }}
+              />
+              <TextField
+                id="phone"
+                label="Phone"
+                type="number"
+                variant="outlined"
+                fullWidth
+                sx={{ margin: "8px" }}
+              />
+
+              <TextareaAutosize
+                aria-label="minimum height"
+                minRows={3}
+                placeholder="Message"
+                style={{ width: "100%", minHeight: "70px", margin: "8px" }}
+              />
+              <Button
+                variant="contained"
+                sx={{ margin: "8px", borderRadius: "10px", width: "150px" }}
+              >
+                Send
+              </Button>
+            </FormControl>
+          </Box>
+        </Grid>
+      </Grid>
+      <Box sx={{ background: "#000", color: COLORS.white, padding: "72px" }}>
+        <Container
+          maxWidth={"md"}
+          disableGutters={false}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <img src={MyLogo} width="500px" height="180px" />
+          </Box>
+          <Box
+            sx={{ display: "flex", justifyContent: "center", padding: "8px" }}
+          >
+            <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+              SERVING LOCAL HEALTHCARE FACILITIES AND HOME PATIENTS
+            </Typography>
+          </Box>
+          <Box
+            sx={{ display: "flex", justifyContent: "center", padding: "16px" }}
+          >
+            <IconButton sx={styles.footerIcons}>
+              <FacebookIcon />
+            </IconButton>
+
+            <IconButton sx={styles.footerIcons}>
+              <InstagramIcon />
+            </IconButton>
+
+            <IconButton sx={styles.footerIcons}>
+              <TwitterIcon />
+            </IconButton>
+
+            <IconButton sx={styles.footerIcons}>
+              <LinkedinIcon />
+            </IconButton>
+          </Box>
+        </Container>
+      </Box>
+      <Box sx={{ background: "#000", color: COLORS.white }}>
+        <Container maxWidth={"lg"} disableGutters={false}>
+          <Typography>© 2023 Nexus Healthcare Staffing</Typography>
+        </Container>
+      </Box>
+    </>
   );
 };
 
