1d889f779bcfa9fc6d369368c5a9525e9c9ffd81
keshav-nandhan <keshavthehellrider@gmail.com>
2023-03-12 15:21:44 +0530

    latest deployment

---
+++ precache-manifest.08a0982f3b86ad166c15e5d990bc9365.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c2064af8fe2794c7d55",
+    "url": "/nexushealthcare/static/css/main.9b037f2a.chunk.css"
+  },
+  {
+    "revision": "7c2064af8fe2794c7d55",
+    "url": "/nexushealthcare/static/js/main.7c2064af.chunk.js"
+  },
+  {
+    "revision": "6c94ac6a6164e001545d",
+    "url": "/nexushealthcare/static/js/1.6c94ac6a.chunk.js"
+  },
+  {
+    "revision": "95d83ac0124efa949917",
+    "url": "/nexushealthcare/static/js/runtime~main.95d83ac0.js"
+  },
+  {
+    "revision": "b1b5f0c0c09ab4b31129eb2961f13a02",
+    "url": "/nexushealthcare/static/media/about_img1.b1b5f0c0.jpg"
+  },
+  {
+    "revision": "290df098ff21799d3971525732e5d1f9",
+    "url": "/nexushealthcare/static/media/action-bg.290df098.jpg"
+  },
+  {
+    "revision": "76c1b2c308acc34b9881074cd65c5a11",
+    "url": "/nexushealthcare/static/media/card1.76c1b2c3.jpg"
+  },
+  {
+    "revision": "87035cff5ab104a766e6a000f74c8ab1",
+    "url": "/nexushealthcare/static/media/card2.87035cff.png"
+  },
+  {
+    "revision": "6793f2e8077ee9490a2ef90b55c1a8f4",
+    "url": "/nexushealthcare/static/media/card3.6793f2e8.jpg"
+  },
+  {
+    "revision": "af12722bc5c6c424e39a654cd827138b",
+    "url": "/nexushealthcare/static/media/card4.af12722b.jpg"
+  },
+  {
+    "revision": "25663d764fa841cf2c20bfabf85e7b3b",
+    "url": "/nexushealthcare/static/media/card5.25663d76.jpg"
+  },
+  {
+    "revision": "345a6d232bcefe56b22e6a0fef119668",
+    "url": "/nexushealthcare/static/media/card6.345a6d23.jpg"
+  },
+  {
+    "revision": "d7f9f1e4929580d7051282eeb870bd2c",
+    "url": "/nexushealthcare/static/media/slide1.d7f9f1e4.jpg"
+  },
+  {
+    "revision": "efc24c5df15befaa93fa57b738f07dcc",
+    "url": "/nexushealthcare/static/media/slide2.efc24c5d.jpg"
+  },
+  {
+    "revision": "3e472304bee25b51bc55ffa01aad6e93",
+    "url": "/nexushealthcare/static/media/slide3.3e472304.jpg"
+  },
+  {
+    "revision": "189514d479817290889a3d69b8754298",
+    "url": "/nexushealthcare/static/media/slide4.189514d4.jpg"
+  },
+  {
+    "revision": "a03742fdc381740316c024c7ae4174d5",
+    "url": "/nexushealthcare/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/nexushealthcare/precache-manifest.08a0982f3b86ad166c15e5d990bc9365.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/nexushealthcare/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
